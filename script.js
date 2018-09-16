be4cf3a4abfcde0261160392b1d18d40470747a0
Anurup Dey <anu.rup.dey98@gmail.com>
2018-09-16 12:37:34 +0530

    Added Files correctly

---
+++ script.js
@@ -0,0 +1,40 @@
+$('.responsive').slick({
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplaySpeed: 1000,
+  autoplay: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        infinite: true,
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
