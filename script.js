d7970bfe5ef7d6272c4a55c5ee182dd4aaaa4a91
Anurup Dey <anu.rup.dey98@gmail.com>
2018-09-18 00:57:10 +0530

    fixed the slider for all resolutions

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 $('.responsive').slick({
   speed: 300,
-  centerMode: true,
   slidesToShow: 5,
   slidesToScroll: 1,
   arrows: true,
   autoplaySpeed: 1000,
   autoplay: true,
   infinite: true,
+  centerMode: true,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1300,
       settings: {
         slidesToShow: 4,
         slidesToScroll: 2,
         infinite: true,
-
+        centerMode: false
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 800,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
+        centerMode: false
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 500,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        centerMode: true
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false
       }
     }
-    
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
